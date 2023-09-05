ebb194193427c551c2d0af9493489413eb596d5f
ShaviMaria <javier.maria.salas@gmail.com>
2023-09-05 11:36:01 -0500

    Added Contanct model

---
+++ src/models/contact.model.ts
@@ -0,0 +1,14 @@
+interface IContact {
+    Id_Contact?: number,
+    Name: string,
+    First_Surname?: string,
+    Second_Surname?: string,
+    Email?: string,
+    Phone_Number: string,
+    Secondary_Phone_Number?: string
+    Company?: string,
+    Favorite: number,
+    User?: number
+}
+
+export default IContact
